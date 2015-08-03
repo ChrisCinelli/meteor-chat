726e581463eddfaf3ce0e9c6f03d279e31d12fa3
Chris Cinelli <chris.cinelli@formativelearning.com>
2015-08-03 05:12:30 -0700

    Make it prettier... so we need prettydate!

--- meteor-chat.js
+++ meteor-chat.js
@@ -16,7 +16,7 @@ if (Meteor.isClient) {
 
   Meteor.subscribe("rooms");
   Meteor.subscribe("messages");
-  Session.setDefault("roomname", "Meteor");
+  Session.setDefault("roomname", "Lobby");
 
   Template.input.events({
     'click .sendMsg': function(e) {
@@ -31,8 +31,7 @@ if (Meteor.isClient) {
 
   _sendMessage = function() {
     var el = document.getElementById("msg");
-    var u = Meteor.user()
-    Messages.insert({user: u.username || (u.profile && u.profile.name) || "MyNameIsNobody", msg: el.value, ts: new Date(), room: Session.get("roomname")});
+    Meteor.call("addMessage", el.value, Session.get("roomname"));
     el.value = "";
     el.focus();
   };
@@ -41,16 +40,34 @@ if (Meteor.isClient) {
     messages: function() {
       return Messages.find({room: Session.get("roomname")}, {sort: {ts: -1}});
     },
-	roomname: function() {
+	  roomname: function() {
       return Session.get("roomname");
     }
   });
-  
+
   Template.message.helpers({
-    timestamp: function() {
-      return this.ts.toLocaleString();
+    likeAction: function() {
+      var mu = Meteor.user();
+
+      return !mu || !_.contains(this.likes, mu._id);
+    },
+    nLikes : function(){
+      return this.likes.length;
     }
   });
+
+  Template.message.events({
+    'click .like': function(e) {
+      var userId = Meteor.user();
+      if(userId) userId = userId._id;
+
+      if (!userId) {
+        alert("Please log in to like this message!");
+        return;
+      }
+      Meteor.call("clickLike", this._id);
+    }
+  });  
 
   Template.rooms.events({
     'click li': function(e) {
@@ -79,10 +96,10 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    Messages.remove({});
+    //Messages.remove({});
     Rooms.remove({});
     if (Rooms.find().count() === 0) {
-      ["Meteor", "JavaScript", "Reactive", "MongoDB"].forEach(function(r) {
+      ["Lobby", "Hairstyles", "BigTrucks", "Gadgets"].forEach(function(r) {
         Rooms.insert({roomname: r});
       });
     }
@@ -101,7 +118,7 @@ if (Meteor.isServer) {
   });
   Messages.deny({
     insert: function (userId, doc) {
-      return (userId === null);
+      return true;
     },
     update: function (userId, doc, fieldNames, modifier) {
       return true;
@@ -110,9 +127,52 @@ if (Meteor.isServer) {
       return true;
     }
   });
-  Messages.allow({
-    insert: function (userId, doc) {
-      return (userId !== null);
+
+  var urlRegex = new RegExp('^(?:(?:http|https)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,}))))(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$', 'i');
+  var imgParserRegex = new RegExp('\{(img)\s*=\s*([^\}]*)\}(.*)','ig');
+  function parseMsg(msg){
+    var result = imgParserRegex.exec(msg);
+    if (!result || result[1] != 'img' || !urlRegex.test(result[2])){
+      return {msg: msg};
+    } else {
+      return {
+        msg : result[3],
+        img : result[2]
+      };
+    }
+  }
+
+  Meteor.methods({
+    addMessage : function (msg, room) {
+      var u = Meteor.user();
+
+      var data = _.extend(parseMsg(msg),{
+        user:  u.username || (u.profile && u.profile.name) || "MyNameIsNobody", 
+        ts:    new Date(), 
+        room:  room, 
+        likes: []
+      });
+
+      Messages.insert(data);      
+    },
+    clickLike : function(msg_id){
+      var userId = Meteor.user();
+      if (userId){
+          userId = userId._id;
+      } else {
+        return false;
+      }
+      var msg = Messages.findOne({_id : msg_id});
+      if(!msg) return false;
+
+      var idx = msg.likes.indexOf(userId);
+      if(idx > -1) {
+        msg.likes.splice(idx, 1);
+      } else {
+        msg.likes.push(userId);
+      }
+      Messages.update({_id : msg_id}, {$set: {likes : msg.likes}});
+      return true;
     }
   });
   
